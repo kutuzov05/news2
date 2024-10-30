eddb305abb02f66d769e67caefa1e3be5a24a537
kutuzov05 <144845962+kutuzov05@users.noreply.github.com>
2024-10-30 16:52:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pygame
+
 from settings import *
 import newspage as np
 
@@ -14,9 +16,26 @@ def display_screen():
         pygame.draw.rect(screen, WHITE, (0, height*0.05+height*0.1*nums+y, width, height*0.1), int(height*0.005))
 
 
+def display_article():
+    screen.fill(BLUE)
+    font = pygame.font.Font(None, int(height * 0.05))
+    text = font.render(str(title), True, WHITE)
+    text_render = text.get_rect()
+    text_render.center = (width*0.5, height*0.05)
+    screen.blit(text, text_render)
+
+
+def check_title():
+    for nums in range(len(np.news)):
+        if height*0.05+height*0.1*nums+y <= mouse_y <= height*0.05+height*0.1*nums+y+height*0.1:
+            return nums
+    return None
+
+
 running = True
 while running:
     clock.tick(FPS)
+    mouse_x, mouse_y = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -25,6 +44,15 @@ while running:
                 y += speed
             if event.button == 5:
                 y -= speed
-    display_screen()
+            if event.button == 1 and type(check_title()) == int:
+                title = np.news[check_title()]["title"]
+                print(title)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE and title is not None:
+                title = None
+    if title is None:
+        display_screen()
+    else:
+        display_article()
     pygame.display.update()
 pygame.quit()
