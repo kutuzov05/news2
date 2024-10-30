b5b1da23a8ed1eb51a9ea51f8c15e3b6d407260f
kutuzov05 <144845962+kutuzov05@users.noreply.github.com>
2024-10-30 16:26:11 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+pygame.init()
+width, height = 500, 500
+screen = pygame.display.set_mode((width, height))
+FPS = 60
+clock = pygame.time.Clock()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (20, 28, 140)
+
+y = 0
+speed = int(height*0.05)
